27be4f6f4ac8292dc729a04fd37207d1bcd58cec
Ogbara Gerrald Godstar <sengodstar@gmail.com>
2020-05-12 02:20:29 +0100

    Remove proxy config and use cors

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -17,16 +17,19 @@ export class AuthService {
   }
 
   register(userDetails) {
-    this.http.post<any>("/api/account", userDetails).subscribe( res => this.authenticate(res));
+    this.http.post<any>("https://houze-api-7375.herokuapp.com/api/account", userDetails).subscribe( res => this.authenticate(res));
+    // this.http.post<any>("/api/account", userDetails).subscribe( res => this.authenticate(res));
   }
 
   login(credentials) {
-    this.http.post<any>("/api/account/login", credentials).subscribe( res => this.authenticate(res));
+    this.http.post<any>("https://houze-api-7375.herokuapp.com/api/account/login", credentials).subscribe( res => this.authenticate(res));
+    // this.http.post<any>("/api/account/login", credentials).subscribe( res => this.authenticate(res));
   }
 
   postHouse(obj) {
     console.log(obj)
-    return this.http.post("/api/houses", obj);
+    // return this.http.post("/api/houses", obj);
+    return this.http.post("https://houze-api-7375.herokuapp.com/api/houses", obj);
   }
 
   authenticate(res) {
@@ -42,49 +45,60 @@ export class AuthService {
   }
 
   getHouses() {
-    return this.http.get("/api/houses");
+    return this.http.get("https://houze-api-7375.herokuapp.com/api/houses");
+    // return this.http.get("/api/houses");
   }
 
   getHouseById(id) {
-    return this.http.get(`/api/houses/${id}`);
+    return this.http.get(`https://houze-api-7375.herokuapp.com/api/houses/${id}`);
+    // return this.http.get(`/api/houses/${id}`);
   }
 
   getHouselikes(houseId) {
-    return this.http.get(`/api/likes/${houseId}`);
+    return this.http.get(`https://houze-api-7375.herokuapp.com/api/likes/${houseId}`);
+    // return this.http.get(`/api/likes/${houseId}`);
   }
 
   postComment(comment) {
-    return this.http.post<any>("/api/comments", comment);
+    return this.http.post<any>("https://houze-api-7375.herokuapp.com/api/comments", comment);
+    // return this.http.post<any>("/api/comments", comment);
   }
 
   getComments(id) {
-    return this.http.get(`/api/comments/${id}`);
+    return this.http.get(`https://houze-api-7375.herokuapp.com/api/comments/${id}`);
+    // return this.http.get(`/api/comments/${id}`);
   }
 
   like(obj) {
-    return this.http.post<any>("/api/likes", obj);
+    return this.http.post<any>("https://houze-api-7375.herokuapp.com/api/likes", obj);
+    // return this.http.post<any>("/api/likes", obj);
   }
 
   unlike(id) {
     console.log(typeof id)
-    this.http.delete(`/api/likes/${id}`).subscribe(res => {
+    this.http.delete(`https://houze-api-7375.herokuapp.com/api/likes/${id}`).subscribe(res => {
+    // this.http.delete(`/api/likes/${id}`).subscribe(res => {
       console.log(res);
     })
   }
 
   getUser() {
-    return this.http.get(`/api/users/`);
+    return this.http.get(`https://houze-api-7375.herokuapp.com/api/users/`);
+    // return this.http.get(`/api/users/`);
   }
 
   getUserHouses(id) {
-    return this.http.get(`/api/users/userhouses/${id}`);
+    return this.http.get(`https://houze-api-7375.herokuapp.com/api/users/userhouses/${id}`);
+    // return this.http.get(`/api/users/userhouses/${id}`);
   }
 
   getUserById(userId) {
-    return this.http.get(`/api/users/${userId}`);
+    return this.http.get(`https://houze-api-7375.herokuapp.com/api/users/${userId}`);
+    // return this.http.get(`/api/users/${userId}`);
   }
 
   updateUser(user) {
-    this.http.put(`/api/users/${user.id}`, user).subscribe(res => console.log(res));
+    this.http.put(`https://houze-api-7375.herokuapp.com/api/users/${user.id}`, user).subscribe(res => console.log(res));
+    // this.http.put(`/api/users/${user.id}`, user).subscribe(res => console.log(res));
   }
 }
